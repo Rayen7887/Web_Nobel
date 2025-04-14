8e4e21572f7adf5088983e0b70da1d9668b46f76
Rayen7887 <nagysandor1215@gmail.com>
2025-04-14 10:03:27 +0200

    megjelenit adat csunyan

--- menu.js
+++ menu.js
@@ -3,20 +3,20 @@ document.getElementById("menu").innerHTML+=`
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Logo</a>
+    <a class="navbar-brand" href="#">Nobel díj</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Nobel díjről tudnivalók</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">2</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">3</a>
         </li>
       </ul>
     </div>
