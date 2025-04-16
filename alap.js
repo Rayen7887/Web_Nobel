673b4951dcf6f4d188f58108d2a0e3bdf0c77a09
Rayen7887 <nagysandor1215@gmail.com>
2025-04-16 10:40:26 +0200

    kartyak letrehozasa

--- alap.js
+++ alap.js
@@ -8,9 +8,11 @@ function megjelenit(y){
     let sz=""
     for (const elem of y.prizes) {
         sz+=`
-        <div>
-        <p>${elem.year}</p>
-        <p>${elem.category}</p>
+        <div class="col-sm-3">
+            <div>
+                <p>${elem.year}</p>
+                <p>${elem.category}</p>
+            </div>
         </div>
         `
     }
--- menu.js
+++ menu.js
@@ -3,14 +3,14 @@ document.getElementById("menu").innerHTML+=`
 
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Nobel díj</a>
+    <a class="navbar-brand" href="https://en.wikipedia.org/wiki/Nobel_Peace_Prize">Nobel díj</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Nobel díjről tudnivalók</a>
+          <a class="nav-link" href="#">Összes díj</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">2</a>
