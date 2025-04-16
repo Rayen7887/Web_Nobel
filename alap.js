e7ec353357ac533a150d08a1bd780270547d2d0e
Rayen7887 <nagysandor1215@gmail.com>
2025-04-16 11:34:16 +0200

    modalis kezdet

--- alap.js
+++ alap.js
@@ -6,18 +6,24 @@ function megjelenit(y){
     console.log(y)
 
     let sz=""
+    let sorszam = 0
     for (const elem of y.prizes) {
         sz+=`
         <div class="col-sm-3">
-            <div>
+            <div id="szegely" data-bs-toggle="modal" data-bs-target="#myModal onclick="reszletkiir(${sorszam})">
                 <p>${elem.year}</p>
                 <p>${elem.category}</p>
             </div>
         </div>
+
         `
+        sorszam++
     }
     document.getElementById("keret").innerHTML=sz
 }
 
+function reszletkiir(sorszam){
+    console.log(sorszam)
+}
 
 
--- menu.js
+++ menu.js
@@ -1,25 +1,27 @@
-document.getElementById("menu").innerHTML+=`
+document.getElementById("menu").innerHTML=`
 
-
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="https://en.wikipedia.org/wiki/Nobel_Peace_Prize">Nobel díj</a>
+    <a class="navbar-brand" href="https://hu.wikipedia.org/wiki/Nobel-d%C3%ADj">Nobel-díj</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Összes díj</a>
+          <a class="nav-link" href="index.html">Összes díj</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">2</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">3</a>
+          <a class="nav-link" href="#">Link</a>
         </li>
       </ul>
     </div>
   </div>
-</nav>
+</nav> 
+
+
+
 `
