df97a64c8107d2a03345efea5d4a24778f3a58c3
Rayen7887 <nagysandor1215@gmail.com>
2025-04-28 08:57:55 +0200

    modalis fejkész

--- alap.js
+++ alap.js
@@ -1,8 +1,11 @@
+let tomb = []
+
 fetch("https://api.nobelprize.org/v1/prize.json")
 .then(x=>x.json())
 .then(y=>megjelenit(y))
 
 function megjelenit(y){
+    tomb = y
     console.log(y)
 
     let sz=""
@@ -10,7 +13,7 @@ function megjelenit(y){
     for (const elem of y.prizes) {
         sz+=`
         <div class="col-sm-3">
-            <div id="szegely" data-bs-toggle="modal" data-bs-target="#myModal onclick="reszletkiir(${sorszam})">
+            <div class="szegely" data-bs-toggle="modal" data-bs-target="#myModal" onclick="reszletkiir(${sorszam})">
                 <p>${elem.year}</p>
                 <p>${elem.category}</p>
             </div>
@@ -24,6 +27,9 @@ function megjelenit(y){
 
 function reszletkiir(sorszam){
     console.log(sorszam)
+    //alert(JSON.stringify(tomb))
+    //console.log(tomb)
+    document.getElementById("modalFej").innerHTML=tomb.prizes[sorszam].year+" "+ tomb.prizes[sorszam].category
 }
 
 
